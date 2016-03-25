a8bb24b0a5c9bddc3ee432211f0d21cc9b315295
Roman <s91630277@gmail.com>
2016-03-25 14:43:09 +0300

    Fixed unit tests (p2)

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,6 @@ module.exports = function(karma) {
     var config = {
         basePath: "./",
         frameworks: [
-            // "systemjs",
             "jasmine"
         ],
         files: [
@@ -18,20 +17,19 @@ module.exports = function(karma) {
              * Do not include tests or libraries.
              * These files will be instrumented by Istanbul.
              */
-            '.karma/**/*!(.spec).js': ['coverage']
+            ".karma/**/*!(.spec).js": ["coverage"]
         },
 
-        /** Optionally, configure the reporter */
         coverageReporter: {
-            type: 'json',
-            subdir: './json',
-            file: 'coverage-js.json'
+            type: "json",
+            subdir: "./json",
+            file: "coverage-js.json"
         },
 
         singleRun: true,
         port: 9876,
         colors: true,
-        logLevel: karma.LOG_DEBUG,
+        logLevel: karma.LOG_INFO,
 
         /**
          * @param browsers {Array} List of browsers for Karma to run the tests against.
@@ -40,50 +38,12 @@ module.exports = function(karma) {
         browsers: ["PhantomJS"],
 
         plugins: [
-            // "karma-systemjs",
             "karma-jasmine",
             "karma-coverage",
             "karma-phantomjs-launcher"
         ],
 
-        // customLaunchers: {
-        //     ChromeTravisCI: {
-        //         base: 'Chrome',
-        //         flags: ['--no-sandbox']
-        //     }
-        // },
-
-        // systemjs: {
-        //     // Path to your SystemJS configuration file
-        //     configFile: "./system.conf.js",
-        //     files: [
-        //         ".karma/**/*.js",
-        //     ],
-
-        //     // Patterns for files that you want Karma to make available, but not loaded until a module requests them. eg. Third-party libraries.
-        //     serveFiles: [
-        //         // "node_modules/**/*.js"
-        //     ],
-
-        //     // SystemJS configuration specifically for tests, added after your config file.
-        //     // Good for adding test libraries and mock modules
-        //     config: {
-        //         paths: {
-        //             // "lodash": "node_modules/lodash/*.js"
-        //             // 'angular-mocks': 'bower_components/angular-mocks/angular-mocks.js'
-        //         }
-        //     }
-        // }
     };
-
-    /**
-     * `PhantomJS2` support is limited in Travis CI so we use `Chrome` instead.
-     * Note that we also need to configure Travis so it enables Chrome.
-     * See `before_script` in the `.travis.yml` file.
-     */
-    if (process.env.TRAVIS) {
-        config.browsers = ['ChromeTravisCI'];
-    }
 
     karma.set(config);
 };
