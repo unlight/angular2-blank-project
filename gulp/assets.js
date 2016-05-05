2cf11771b2fc251a53917c5946f7e1eed69fbff4
roman.vasilev <s91630277@gmail.com>
2016-05-06 00:35:06 +0300

    Removed assets libs since we serve from node_modules

--- gulp/assets.js
+++ gulp/assets.js
@@ -1,4 +1,3 @@
-const combine = require("stream-combiner");
 const merge2 = require("merge2");
 
 module.exports = (gulp, g, config, debug) => {
@@ -6,14 +5,7 @@ module.exports = (gulp, g, config, debug) => {
     gulp.task("assets", function assets() {
         var images = gulp.src("src/images/**/*.{png,jpg,gif,svg}")
             .pipe(gulp.dest("build/design/images"));
-        var jsLibs = gulp.src(config.jsLibs, {base: "node_modules"});
-        var libs = merge2(jsLibs)
-            .pipe(g.if(config.isProd, combine(
-                g.concat("libs.js"),
-                g.uglify({mangle: false})
-            )))
-            .pipe(gulp.dest("build/libs"));
-        return merge2([images, libs]);
+        return merge2([images]);
     });
 
 };
--- gulp/htdocs.js
+++ gulp/htdocs.js
@@ -1,17 +1,20 @@
 const path = require("path");
 
 module.exports = (gulp, g, config, debug, _) => {
-  
+
     gulp.task("htdocs", function htdocs() {
-        var styles = ["build/design/style.css", "build/design/*"];
-        var jsLibs = config.isProd ? ["build/libs/*"] : config.jsLibs.map(lib => path.join("build/libs", path.relative("node_modules", lib)));
-        var source = gulp.src([...styles, ...jsLibs], { read: false })
-                .pipe(debug("Injecting"));
+        var styles = gulp.src(["build/design/style.css", "build/design/*"], { read: false });
+        var jsLibs = config.jsLibs;
+        if (config.isProd) {
+           jsLibs = ["build/libs/*"];
+        }
+        console.log('jsLibs ' , jsLibs);
+        var scripts = gulp.src(jsLibs, { read: false });
         return gulp.src("src/index.html")
-            .pipe(g.inject(source, { addRootSlash: false, ignorePath: "build" }))
+            .pipe(g.inject(styles, { addRootSlash: false, ignorePath: "build" }))
+            .pipe(g.inject(scripts, { addRootSlash: false, ignorePath: "build" }))
             .pipe(g.preprocess({ context: config }))
             .pipe(gulp.dest("build"))
             .pipe(g.connect.reload());
     });
-
 };
