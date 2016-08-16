b03a912751cdd8f9350e1a030a2fd9d2627edc98
Roman Vasilev <Roman Vasilev>
2016-08-16 23:04:51 +0300

    Removed AppComponent from main.ts

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,7 +1,6 @@
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app.module';
-import {AppComponent} from './components/app/app.component';
+import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
 
@@ -9,7 +8,7 @@ platformBrowserDynamic().bootstrapModule(AppModule);
 // @if isProd
 enableProdMode();
 // The browser platform without a compiler
-import { platformBrowser } from '@angular/platform-browser';
+// import { platformBrowser } from '@angular/platform-browser';
 // The app module factory produced by the static offline compiler
 // import { AppModuleNgFactory } from './app.module.ngfactory';
 // Launch with the app module factory.
