3c76dc30c7c7222717d89bd5017372fc244ab244
Roman Vasilev <Roman Vasilev>
2016-08-20 01:28:11 +0300

    Edit component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,13 @@ import {provideRouter, RouterConfig} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {AboutComponent} from './components/about/about.component';
 import {SearchComponent} from './components/search/search.component';
+import { EditComponent } from './components/edit/edit.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'search', component: SearchComponent },
+    { path: 'edit/:id', component: EditComponent },
     // { path: '**', component: PageNotFoundComponent }
 ];
 
