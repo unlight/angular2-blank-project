d37fb1459b8b57c7fcfbdde115cb9d1409c8fb0f
Roman Vasilev <s91630277@gmail.com>
2016-08-23 16:32:19 +0300

    Abstracted getDom

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -4,34 +4,36 @@ import {Component} from '@angular/core';
 import {HomeComponent} from './home.component';
 import {NameListService} from '../../services/name-list.service';
 import { getDOM } from '@angular/platform-browser/src/dom/dom_adapter';
-// import {disableDeprecatedForms, provideForms} from '@angular/forms';
+import {__platform_browser_private__ as platformBrowser} from '@angular/platform-browser';
+import {FORM_DIRECTIVES} from '@angular/forms';
 
 describe('Home component', () => {
 
-    // TODO: It looks like you're using the old forms module
     it('should work', async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
-        return tcb.createAsync(TestComponent).then(rootTC => {
-            rootTC.detectChanges();
+        return tcb.createAsync(TestComponent)
+            .then(rootTC => {
+                rootTC.detectChanges();
 
-            let homeInstance = rootTC.debugElement.children[0].componentInstance;
-            let homeDOMEl = rootTC.debugElement.children[0].nativeElement;
-            let nameListLen = function () {
-                return homeInstance.nameListService.names.length;
-            };
-            expect(homeInstance.nameListService).toEqual(jasmine.any(NameListService));
-            expect(nameListLen()).toEqual(4);
-            expect(getDOM().querySelectorAll(homeDOMEl, 'li').length).toEqual(nameListLen());
-            // But we do not need it.
-            expect(homeDOMEl.querySelectorAll('li').length).toEqual(nameListLen());
+                let homeInstance = rootTC.debugElement.children[0].componentInstance;
+                let homeDOMEl = rootTC.debugElement.children[0].nativeElement;
+                let nameListLen = function () {
+                    return homeInstance.nameListService.names.length;
+                };
+                expect(homeInstance.nameListService).toEqual(jasmine.any(NameListService));
+                expect(nameListLen()).toEqual(4);
+                expect(platformBrowser.getDOM().querySelectorAll(homeDOMEl, 'li').length).toEqual(nameListLen());
+                // But we do not need it.
+                expect(homeDOMEl.querySelectorAll('li').length).toEqual(nameListLen());
 
-            homeInstance.newName = 'Minko';
-            homeInstance.addName();
-            rootTC.detectChanges();
+                homeInstance.newName = 'Minko';
+                homeInstance.addName();
+                rootTC.detectChanges();
 
-            expect(nameListLen()).toEqual(5);
-            expect(getDOM().querySelectorAll(homeDOMEl, 'li').length).toEqual(nameListLen());
-            expect(getDOM().querySelectorAll(homeDOMEl, 'li')[4].textContent).toEqual('Minko');
-        });
+                expect(nameListLen()).toEqual(5);
+                expect(platformBrowser.getDOM().querySelectorAll(homeDOMEl, 'li').length).toEqual(nameListLen());
+                expect(platformBrowser.getDOM().querySelectorAll(homeDOMEl, 'li')[4].textContent).toEqual('Minko');
+            })
+            .catch(err => fail(err));
     })));
 });
 
@@ -39,6 +41,6 @@ describe('Home component', () => {
     providers: [NameListService],
     selector: 'test-cmp',
     template: '<sd-home></sd-home>',
-    directives: [HomeComponent]
+    directives: [HomeComponent, FORM_DIRECTIVES]
 })
 class TestComponent { }
