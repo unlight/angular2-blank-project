53a264318a87650ffc1435b361b4b28e7697de26
Roman Vasilev <Roman Vasilev>
2016-08-25 23:26:38 +0300

    Moved build string to conf

--- gulp/htdocs.js
+++ gulp/htdocs.js
@@ -1,7 +1,7 @@
-module.exports = (gulp, g, config) => {
+module.exports = (gulp, g, paths, config) => {
 
     gulp.task("htdocs", function htdocs() {
-        var styles = gulp.src(["build/design/main.css", "build/design/*"], { read: false });
+        var styles = gulp.src(["build/design/main.css", "build/design/*.css"], { read: false });
         var jsLibs = config.jsLibs;
         if (config.isProd) {
             jsLibs = "build/js/*.js";
@@ -9,10 +9,10 @@ module.exports = (gulp, g, config) => {
         var scripts = gulp.src(jsLibs, { read: false })
             .pipe(g.if(config.isProd, g.order(["polyfills.js", "vendors.js", "main.js", "*.js"])));
         return gulp.src("src/index.html")
-            .pipe(g.inject(styles, { addRootSlash: false, ignorePath: "build" }))
-            .pipe(g.inject(scripts, { addRootSlash: false, ignorePath: "build" }))
+            .pipe(g.inject(styles, { addRootSlash: false, ignorePath: paths.dest }))
+            .pipe(g.inject(scripts, { addRootSlash: false, ignorePath: paths.dest }))
             .pipe(g.preprocess({ context: config }))
-            .pipe(gulp.dest("build"))
+            .pipe(gulp.dest(paths.dest))
             .pipe(g.connect.reload());
     });
 };
