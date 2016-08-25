bf15ee17addc5276cee591ce5c060f6d810cfe43
Roman Vasilev <Roman Vasilev>
2016-08-25 23:20:18 +0300

    Promise.all

--- gulp/symlinks.js
+++ gulp/symlinks.js
@@ -5,7 +5,9 @@ module.exports = (gulp, config) => {
             return done();
         }
         var link = require("fs-symlink");
-        return link("node_modules", "build/node_modules", "junction");
+        return Promise.all([
+            link("node_modules", "build/node_modules", "junction"),
+        ]);
     });
 
 };
