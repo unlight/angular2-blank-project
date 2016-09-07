5d13120bff44c0b920be16c28d11d8ec303ac03d
Roman Vasilev <Roman Vasilev>
2016-09-07 22:26:50 +0300

    Fixed som simple tests

--- src/app/services/mocks/search.service.ts
+++ src/app/services/mocks/search.service.ts
@@ -13,10 +13,10 @@ export class MockSearchService {
     constructor() {
         this.spy = jasmine.createSpy;
         this.fakeResponse = null;
-        this.getAllSpy = this.spy('getAll').andReturn(this);
-        this.getByIdSpy = this.spy('get').andReturn(this);
-        this.searchSpy = this.spy('search').andReturn(this);
-        this.saveSpy = this.spy('save').andReturn(this);
+        this.getAllSpy = this.spy('getAll').and.returnValue(this);
+        this.getByIdSpy = this.spy('get').and.returnValue(this);
+        this.searchSpy = this.spy('search').and.returnValue(this);
+        this.saveSpy = this.spy('save').and.returnValue(this);
     }
 
     subscribe(callback: Function | any) {
--- src/app/services/name-list.service.spec.ts
+++ src/app/services/name-list.service.spec.ts
@@ -1,14 +1,14 @@
-// import {NameListService} from './name-list.service';
+import {NameListService} from './name-list.service';
 
-// describe('NameList Service', () => {
-//     let nameListService: NameListService;
+describe('NameList Service', () => {
+    let nameListService: NameListService;
 
-//     beforeEach(() => {
-//         nameListService = new NameListService;
-//     });
+    beforeEach(() => {
+        nameListService = new NameListService;
+    });
 
-//     it('should return the list of names', () => {
-//         let names = nameListService.get();
-//         expect(names).toEqual(jasmine.any(Array));
-//     });
-// });
+    it('should return the list of names', () => {
+        let names = nameListService.get();
+        expect(names).toEqual(jasmine.any(Array));
+    });
+});
