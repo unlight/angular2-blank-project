66de0177bd7cb4f14f6fed7b1c8992373c28b735
iamthes <s91630277@gmail.com>
2016-09-28 22:10:15 +0300

    Mov

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,5 @@
-import {AppModule} from './app.module';
 import {enableProdMode} from '@angular/core';
+import {AppModule} from './app.module';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 // @if isProd
